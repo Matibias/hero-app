c9ca95cdb3b0aa6da2de783a656f217bfe80ed27
Matias Parra <mparra@retailsbs.com>
2023-03-20 23:36:13 -0300

    refactor: heroesroutes in AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-
-import { Navbar } from '../ui'
+import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from "../auth"
-import { MarvelPage, DcPage } from '../heroes'
+import { HeroesRoutes } from '../heroes'
 
 
 export const AppRouter = () => {
   return (
     <>
 
-      <Navbar />
-
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
 
         <Route path="login" element={<LoginPage />} />
+        
+        <Route path="/*" element={<HeroesRoutes />} />
 
-        <Route path="/" element={<Navigate to={'/marvel'} />} />
       </Routes>
     </>
   )
