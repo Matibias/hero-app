612656e6dd40cb9d884c1243fb14d6c22803a943
Matias Parra <mparra@retailsbs.com>
2023-03-25 21:54:24 -0300

    refact: useSearch

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -5,28 +5,11 @@ import queryString from 'query-string'
 import { useForm } from "../../hook/useForm";
 import { HeroCard } from "../components";
 import { getHeroByName } from "../helpers";
+import { useSearch } from "../hooks/useSearch";
 
 export const SearchPage = () => {
 
-  const navigate = useNavigate() // para obtener la navegacion
-  const location = useLocation(); // para obtener la localizacion de donde estamos (query)
-
-  const {q = ''} = queryString.parse(location.search) // queryString para parsear la query
-  const heroes = getHeroByName(q)
-
-  const showSearch = (q.length === 0)
-  const showError = (q.length > 0) && (heroes.length === 0)
-
-  const { searchText, onInputChange } = useForm({
-    searchText: q
-  });
-
-  const onSearchSubmit = (event) => {
-    event.preventDefault();
-    // if (searchText.trim() <=1) return;
-
-    navigate(`?q=${ searchText.toLowerCase().trim()}`)
-  }
+  const {  showSearch, showError, onInputChange, onSearchSubmit, searchText, heroes, q } = useSearch()
 
   return (
     <>
